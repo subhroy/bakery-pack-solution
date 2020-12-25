e43c8b654e36baa41931a5791ac2c8088006682d
subhroy <subhasis.it@gmail.com>
2020-12-26 01:36:42 +0530

    Test case Added

--- src/test/java/BakerySolutionTest.java
+++ src/test/java/BakerySolutionTest.java
@@ -7,5 +7,7 @@ public class BakerySolutionTest {
     @Test
     public void calculateOutputTest(){
         Assert.assertEquals("9,9,5,5,3,3",BakerySolution.calculateOutput(34));
+        //Assert.assertEquals("9,3",BakerySolution.calculateOutput(12));
+        //Assert.assertEquals("5,5",BakerySolution.calculateOutput(10));
     }
 }
